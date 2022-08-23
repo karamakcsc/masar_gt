734053389904edcf07230ec340c28dc79638ee5d
karamakcsc <karamakcsc@hotmail.com>
2022-08-23 13:58:01 +0000

    First_Upload

--- masar_gt/hooks.py
+++ masar_gt/hooks.py
@@ -154,24 +154,24 @@ doctype_js = {
 # --------------------
 
 user_data_fields = [
-	{
-		"doctype": "{doctype_1}",
-		"filter_by": "{filter_by}",
-		"redact_fields": ["{field_1}", "{field_2}"],
-		"partial": 1,
-	},
-	{
-		"doctype": "{doctype_2}",
-		"filter_by": "{filter_by}",
-		"partial": 1,
-	},
-	{
-		"doctype": "{doctype_3}",
-		"strict": False,
-	},
-	{
-		"doctype": "{doctype_4}"
-	}
+    {
+    "doctype": "{doctype_1}",
+    "filter_by": "{filter_by}",
+    "redact_fields": ["{field_1}", "{field_2}"],
+    "partial": 1,
+    },
+    {
+    "doctype": "{doctype_2}",
+    "filter_by": "{filter_by}",
+    "partial": 1,
+    },
+    {
+    "doctype": "{doctype_3}",
+    "strict": False,
+    },
+    {
+    "doctype": "{doctype_4}"
+    }
 ]
 
 # Authentication and authorization
